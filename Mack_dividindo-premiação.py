a083dbb57bc3294b509dae77c7726e4e4fb69508
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-08-17 00:19:23 -0300

    Programa em Python mostrando três ganhadores dividindo o prêmio em porcentagens diferentes após um sorteio.

---
+++ Mack_dividindo-premiação.py
@@ -0,0 +1,10 @@
+premioMontante = 780000.00
+jogador1 = 46
+jogador2 = 32
+jogador3 = 22
+ganhador1 = (jogador1)*(premioMontante)/100 
+print(ganhador1)
+ganhador2 = (jogador2)*(premioMontante)/100
+print(ganhador2)
+ganhador3 = (jogador3)*(premioMontante)/100
+print(ganhador3)
---
+++ Mack_python-numerosInteiros.py
@@ -0,0 +1,28 @@
+'''print('Digite o primeiro número:')
+numero1 = int(input())
+print('Digite o segundo número:')
+numero2 = int(input())
+resultado = (numero1*numero2)/2
+print('O quadrado da diferença do primeiro valor pelo segundo é', resultado)'''
+
+print('Digite um número:')
+user1 = int(input())
+print('Digite outro número:')
+user2 = int(input())
+soma = (user1+user2)/5
+print(f'A divisão com base na soma das variáveis é {soma}', 'e a multiplicação do resultado da variável soma menos o valor de 5 é igual a', soma *3-5)
+
+print('Digite um número:')
+user1 = int(input())
+print('Digite outro número:')
+user2 = int(input())
+soma = (user1+user2)/5
+print(f'A divisão com base na soma das variáveis é {soma}', 'e a multiplicação do resultado da variável soma menos o valor de 5 é igual a', soma *3-5)
+
+print('Digite um número:')
+user1 = int(input())
+print('Digite outro número:')
+user2 = int(input())
+soma = (user1+user2)/5
+print(f'A divisão com base na soma das variáveis é {soma}', 'e a multiplicação do resultado da variável soma menos o valor de 5 é igual a', soma *3-5)
+

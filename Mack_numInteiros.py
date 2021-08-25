a206d4b496f90a56955eef8ed67e1060e65a9981
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-08-24 23:40:12 -0300

    Mack_pythonClass: Encontrando o quadrado da diferença de dois números inteiros

---
+++ Mack_numInteiros.py
@@ -0,0 +1,28 @@
+print('Digite um numero inteiro:')
+inteiroDigitado1 = int(input())
+print('Digite outro numero inteiro:')
+inteiroDigitado2 = int(input())
+
+exibeQuadrado = (inteiroDigitado1*inteiroDigitado2) /2
+print(exibeQuadrado)
+
+
+
+
+
+
+
+
+'''soma = numero1+numero2
+subtracao = numero1-numero2
+multiplicacao = numero1*numero2
+divisao = numero1/numero2
+divrun = numero1//numero2
+rest = numero1%numero2
+expo = numero1**numero2
+print(soma)
+print(subtracao)
+print(divisao)
+print(divrun)
+print(rest)
+print(expo)'''
